c496838ceecb7c76343d3f0dfc6ba373e1120613
FerrariFlunker <ferrariflunker@gmail.com>
2022-01-01 16:34:12 -0600

    add user fields functionality

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class DiscourseChatIntegration::Rule < DiscourseChatIntegration::PluginModel
+  @@MSG_FIELDS = {'{username}' => post.user.username, '{title}' =>  post.topic.title, "{category}" => post.topic.category.name}
+
   # Setup ActiveRecord::Store to use the JSON field to read/write these values
   store :value, accessors: [ :channel_id, :type, :group_id, :category_id, :tags, :filter, :new_topic_prefix, :new_reply_prefix ], coder: JSON
 
@@ -47,6 +49,10 @@ class DiscourseChatIntegration::Rule < DiscourseChatIntegration::PluginModel
                                 message: "%{value} is not a valid filter" }
 
   validate :channel_valid?, :category_valid?, :group_valid?, :tags_valid?
+
+  def MSG_FIELDS
+    @@MSG_FIELDS
+  end
 
   def self.key_prefix
     'rule:'.freeze
