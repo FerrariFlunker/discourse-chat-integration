52fd34359fc0778d80b179e4c702d4fd57e4e2b8
FerrariFlunker <ferrariflunker@gmail.com>
2022-01-02 00:10:42 -0600

    fixed constant

--- lib/discourse_chat_integration/provider/discord/discord_provider.rb
+++ lib/discourse_chat_integration/provider/discord/discord_provider.rb
@@ -70,11 +70,11 @@ module DiscourseChatIntegration
       end
 
       def self.build_prefix_message(post, rule)
-        MSG_FIELDS = {'{username}' => post.user.username, '{title}' =>  post.topic.title, "{category}" => post.topic.category.name}
+        msg_fields = {'{username}' => post.user.username, '{title}' =>  post.topic.title, "{category}" => post.topic.category.name}
         if post.is_first_post? && rule.new_topic_prefix
-          return rule.new_topic_prefix.gsub(/{(.*?)}/, MSG_FIELDS)
+          return rule.new_topic_prefix.gsub(/{(.*?)}/, msg_fields)
         elsif !post.is_first_post? && rule.new_reply_prefix
-          return rule.new_reply_prefix.gsub(/{(.*?)}/, MSG_FIELDS)
+          return rule.new_reply_prefix.gsub(/{(.*?)}/, msg_fields)
         else
           return ""
         end
