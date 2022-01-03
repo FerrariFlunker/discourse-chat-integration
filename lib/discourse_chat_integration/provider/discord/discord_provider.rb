8e2901b1493064ba26c9c164883717c7a814fbad
FerrariFlunker <ferrariflunker@gmail.com>
2022-01-02 20:31:24 -0600

    add timestamp and footer

--- lib/discourse_chat_integration/provider/discord/discord_provider.rb
+++ lib/discourse_chat_integration/provider/discord/discord_provider.rb
@@ -66,7 +66,12 @@ module DiscourseChatIntegration
             fields: [{
               name: "Category:",
               value: "#{category}"
-            }]
+            }],
+            footer: {
+              text: "aloha.pk",
+              icon_url: "https://community.aloha.pk/uploads/default/original/1X/a740f07af5d758ce95531052bf73bf7fd9f8b7c6.png"              
+            },
+            timestamp: DateTime.now.strftime('%Y-%m-%dT%H:%M:%S.%L%z')
           }]
         }
 
